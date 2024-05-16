3b908a4b032d7688d8ac655c50929469d60f4377
Juan <feanorjm@gmail.com>
2024-05-16 10:16:29 -0400

    changes 122rew

--- main/hubspot/conf.py
+++ main/hubspot/conf.py
@@ -2,7 +2,4 @@ from hubspot import HubSpot
 
 
 def conn():
-    #   api_client = HubSpot(api_key='f45a3ae7-55ca-43a8-801a-ddc71798f037')
-    #api_client = HubSpot()
-    #   api_client = HubSpot(api_key='c3d667b9-c39a-49af-949a-9e05c3170a7c') //developer "hubspot_owner_id": "188375142"
     return api_client
